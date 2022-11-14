624dc9986ed04c886cfc0cbff7a979439f04373b
briares <briares@gmail.com>
2022-11-14 23:43:14 +0100

    deploy to App Engine and add helth check

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/briares/hello-api/handlers"
+	"github.com/briares/hello-api/handlers/rest"
+)
+
+func main() {
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = ":8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck) //
+
+	log.Printf("listening on %s\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
